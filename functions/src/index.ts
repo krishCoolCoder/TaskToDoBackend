dd7fd766f473a06865d040a5d7e910b6deb98f3e
Saikrishna <saikrishna.p@iorta.in>
2024-02-14 10:13:35 +0530

    First commit

---
+++ functions/src/index.ts
@@ -0,0 +1,55 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.get('/', (req: any, res: any) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.send(`
+      <!doctype html>
+      <head>
+        <title>Time</title>
+        <link rel="stylesheet" href="/style.css">
+        <script src="/script.js"></script>
+      </head>
+      <body>
+        <p>In London, the clock strikes:
+          <span id="bongs">${'BONG '.repeat(hours)}</span></p>
+        <button onClick="refresh(this)">Refresh</button>
+      </body>
+    </html>`);
+  });
+
+  app.get('/api', (req: any, res: any) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+  });
+  app.get('/test', (req: any, res: any) => {
+    res.json({message: 'Hello world. ',task: "Its time to go ahead with your task."});
+  });
+  app.get('/love', (req: any, res: any) => {
+    res.json({message: 'Hello world. ',task: "Its time to go ahead with your task."});
+  });
+
+  exports.app = functions.https.onRequest(app);
+
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import {onRequest} from "firebase-functions/v2/https";
+// import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
