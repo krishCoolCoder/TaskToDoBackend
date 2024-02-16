95c4e79086de1ad5baeebf23f6992d8e6b5202fc
Saikrishna <saikrishna.p@iorta.in>
2024-02-16 18:57:16 +0530

    added the fail condition for login

--- functions/src/controller/login/loginController.ts
+++ functions/src/controller/login/loginController.ts
@@ -5,6 +5,8 @@ app.get('/login', (req : any, res : any) => {
     if (req.query.userId != undefined && req.query.userId !== "" && req.query.userId) {
         if (req.query.userId == "saikrishnatechno@gmail.com") {
             res.send("Successfully authenticated.")
+        } else {
+            res.send("User authentication failed."); 
         }
     } else {
         res.send("User authentication failed."); 
