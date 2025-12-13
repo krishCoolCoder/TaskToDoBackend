9a2f9cc7cbf01c4c0f8615d898fe371801469d3e
Saikrishna.io <saikrishna.p@iortatextnxt.com>
2025-12-13 22:08:35 +0530

    Lets do it again

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+// import { PrismaClient } from '@prisma/client';
+// import { headstart } from './startup/headstart'
+import {connectDB} from "./dbCoonection"
+// import { authorisationMiddleware } from './src/middleware/AuthenticationMiddleware';
+const app = express();
+const port = 3000;
+connectDB();
+
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
